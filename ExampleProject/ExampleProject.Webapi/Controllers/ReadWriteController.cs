6efe0e2be7e810afda7b1fd0d54bfaf995def16d
avidakov1 <avidakov1@gmail.com>
2020-07-15 10:17:44 +0200

    Ispravci za dan 2.

--- ExampleProject/ExampleProject.Webapi/Controllers/ReadWriteController.cs
+++ ExampleProject/ExampleProject.Webapi/Controllers/ReadWriteController.cs
@@ -11,105 +11,93 @@ namespace ExampleProject.Webapi.Controllers
     
     public class EmployeeInfo
     {
-        public string Name { get; set; }
-        public string WorkPosition { get; set; }
-        public float MonthlyPay { get; set; }
+        public string name { get; set; }
+        public string workPosition { get; set; }
+        public float monthlyPay { get; set; }
     }
     public class Employee
     {
-        public string Id;
-        public string Name = "John Doe";
-        public string WorkPosition = "Salesman";
-        public float MonthlyPay = 3000;
-        public Employee(string Id) { this.Id = Id; }
-        public Employee(string Id, string Name, string WorkPosition, float MonthlyPay) { this.Id = Id; this.Name = Name; this.WorkPosition = WorkPosition; this.MonthlyPay = MonthlyPay; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string workPosition { get; set; }
+        public float monthlyPay { get; set; }
+        public Employee(string id) { this.id = id; }
+        public Employee(string id, string name, string workPosition, float monthlyPay) { this.id = id; this.name = name; this.workPosition = workPosition; this.monthlyPay = monthlyPay; }
 
-        public bool MyId(string Id) => this.Id == Id;
-        public string Next() => (Int32.Parse(this.Id) + 1).ToString();
         public void ReplaceInfo(EmployeeInfo EI)
         {
-            this.Name = EI.Name;
-            this.WorkPosition = EI.WorkPosition;
-            this.MonthlyPay = EI.MonthlyPay;
-        }
-    }
-
-    public static class Employees
-    {
-        public static List<Employee> Emps = new List<Employee>();
-        static Employees() { }
-        public static void AddEmp(Employee Emp) => Emps.Add(Emp);
-
-        public static List<Employee> getEmps()
-        {
-            return Emps;
-        }
-        public static void setEmps(List<Employee> newEmps)
-        {
-            Emps = newEmps;
-        }
-        public static int changeEmp(string Id, EmployeeInfo values)
-        {
-            int T = Emps.FindIndex(r => r.MyId(Id));
-            Emps[T].ReplaceInfo(values);
-            return T;
-        }
-
-        public static bool deleteId(string Id)
-        {
-            List<Employee> OldEmps = new List<Employee>(Emps);
-            Emps.RemoveAll(r => r.MyId(Id));
-            return OldEmps != Emps;
+            this.name = EI.name;
+            this.workPosition = EI.workPosition;
+            this.monthlyPay = EI.monthlyPay;
         }
     }
     
     public class ReadWriteController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        static public List<Employee> empleyees = new List<Employee> { new Employee("0", "Tim", "Banker", 10000) };
+        [HttpGet]
+        [Route("api/getall")]
+        public HttpResponseMessage Get()
         {
-            return Employees.getEmps();
-        }
-
-        public Employee Get(string Id)
-        {
-            foreach (Employee employee in Employees.getEmps())
+            if (empleyees.Count > 0)
             {
-                if (employee.MyId(Id))
+                return Request.CreateResponse(HttpStatusCode.OK, empleyees);
+            }
+            else 
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "List is empty");
+            }
+        }
+        [HttpGet]
+        [Route("api/getone/{id:int}")]
+        public HttpResponseMessage Get(int id)
+        {
+            foreach (Employee employee in empleyees)
+            {
+                if (employee.id == id.ToString())
                 {
-                    return employee;
+                    return Request.CreateErrorResponse(HttpStatusCode.OK, employee.name+" \n "+employee.workPosition+" \n "+employee.monthlyPay);
                 }
             }
-            Request.CreateResponse(HttpStatusCode.BadRequest, "Bad Request");
-            return null;
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "Bad Request");
         }
 
         // POST: api/ReadWrite
-        [HttpPost]
+        [Route("api/add")]
         public HttpResponseMessage Post([FromBody]EmployeeInfo value)
         {
             if (value != null)
             {
-                int Id = Employees.getEmps().Count;
-                Employee newEmp = new Employee(Id.ToString(), value.Name, value.WorkPosition, value.MonthlyPay);
-                Employees.AddEmp(newEmp);
+                int Id = empleyees.Count;
+                Employee newEmp = new Employee(Id.ToString(), value.name, value.workPosition, value.monthlyPay);
+                empleyees.Add(newEmp);
                 return Request.CreateErrorResponse(HttpStatusCode.OK, "OK");
             }
             return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
         }
 
-        public HttpResponseMessage Put(string id, [FromBody]EmployeeInfo value)
+        [Route("api/update/{id:int}")]
+        public HttpResponseMessage Put([FromUri] int id, [FromBody]EmployeeInfo value)
         {
-            int t = Employees.changeEmp(id, value);
-            if(t!= -1)
+            int iterator = 0;
+            foreach (Employee employee in empleyees)
             {
-                return Request.CreateErrorResponse(HttpStatusCode.OK, "OK");
-            }
-            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "NotFound");
-        }
+                if (employee.id == id.ToString())
+                {
+                    empleyees[iterator].ReplaceInfo(value);
+                }
+                iterator = iterator + 1; 
+                return Request.CreateResponse(HttpStatusCode.OK, "Change made successfully.");
 
-        public HttpResponseMessage Delete(string id)
+            }
+            return Request.CreateResponse(HttpStatusCode.BadRequest, "Bad Request");
+        }
+        [Route("api/remove/{id:int}")]
+        public HttpResponseMessage Delete([FromUri] int id)
         {
-            if (Employees.deleteId(id))
+            var OldEmps = new List<Employee>(empleyees);
+            empleyees.RemoveAll(r => r.id==id.ToString());
+            if (OldEmps != empleyees)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.OK, "OK");
             }
